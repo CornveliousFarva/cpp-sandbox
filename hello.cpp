6b546acc500f4e22ebe61f320b2439160358ad44
Greg Schuman <gcschuman@hotmail.com>
2022-10-28 12:55:14 -0700

    Made my first program in C++, simply saying Hi Everebody.

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+// Programs that outputs "Hi Everybody when run"
+
+int main() {
+
+    std::cout << "Hi Everybody! \n";
+
+    return 0;
+}
+
+// Walkthough
+// Every C++ program has to have a function called main(), which houses all of the functions of the program
+// std::cout-cout ("see out") sends the text out to the terminal to be printed
+// return-ends the function
